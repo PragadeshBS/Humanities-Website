b5070928417fb61c8f10d2caee1da6485b5b7d33
Pragadesh BS <pragadeshbs@protonmail.com>
2022-05-01 13:32:02 +0530

    update navbar

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -12,33 +12,28 @@ const Header = () => {
           </div>
           <div className="ms-3 title">
             <span className="h4">
-              Department of Applied Sciences & Humanities
+              Department of Applied Sciences &amp; Humanities
             </span>
             <span className="h5">MIT Campus, Anna University</span>
           </div>
         </div>
       </div>
       {/* Navbar */}
-      <nav className="navbar navbar-expand-lg navbar-dark accent-bg">
-        <div className="container-fluid">
+      <nav className="navbar navbar-expand-xl navbar-dark accent-bg">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            DASH
+          </Link>
           <button
-            className="navbar-toggler"
+            className="navbar-toggler navbar-toggler-right"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            data-bs-target="#navbar1"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/">
-                  Home
-                </Link>
-              </li>
+          <div className="collapse navbar-collapse" id="navbar1">
+            <ul className="navbar-nav">
               <li className="nav-item">
                 <Link className="nav-link" aria-current="page" to="/hod">
                   HOD Desk
@@ -46,30 +41,72 @@ const Header = () => {
               </li>
               <li className="nav-item dropdown">
                 <a
-                  className="nav-link dropdown-toggle"
                   href="#"
-                  id="navbarDropdown"
-                  role="button"
+                  id="menu"
                   data-bs-toggle="dropdown"
-                  aria-expanded="false"
+                  className="nav-link dropdown-toggle"
+                  data-bs-display="static"
                 >
-                  Faculty
+                  People
                 </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <Link className="dropdown-item" to="/faculty/teaching">
-                      Teaching Staff
-                    </Link>
+                <ul className="dropdown-menu">
+                  <li className="dropdown-submenu">
+                    <a
+                      href="#"
+                      data-bs-toggle="dropdown"
+                      className="dropdown-item dropdown-toggle"
+                    >
+                      Faculty
+                    </a>
+                    <ul className="dropdown-menu">
+                      <li>
+                        <Link
+                          to="/faculty/teaching"
+                          className="dropdown-item"
+                          onClick={(e)=>{console.log(e)}}                          
+                        >
+                          Teaching Staff
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="/faculty/non-teaching"
+                          className="dropdown-item"
+                        >
+                          Non-Teaching Staff
+                        </Link>
+                      </li>
+                    </ul>
                   </li>
-                  <li>
-                    <Link className="dropdown-item" to="/faculty/non-teaching">
-                      Non-Teaching Staff
-                    </Link>
+                  <li className="dropdown-submenu">
+                    <a
+                      href="#"
+                      data-bs-toggle="dropdown"
+                      className="dropdown-item dropdown-toggle"
+                    >
+                      Students
+                    </a>
+                    <ul className="dropdown-menu">
+                      <li>
+                        <a href="#" className="dropdown-item">
+                          M.Phil Mathematics
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" className="dropdown-item">
+                          M.Sc Mathemathics
+                        </a>
+                      </li>
+                    </ul>
                   </li>
                 </ul>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/infrastructure">
+                <Link
+                  className="nav-link"
+                  aria-current="page"
+                  to="/infrastructure"
+                >
                   Infrastructure
                 </Link>
               </li>
@@ -79,22 +116,25 @@ const Header = () => {
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/students">
-                  Students
-                </Link>
-              </li>
-              <li className="nav-item">
                 <Link className="nav-link" aria-current="page" to="/research">
                   Research
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="characterization-facilities">
+                <Link
+                  className="nav-link"
+                  aria-current="page"
+                  to="characterization-facilities"
+                >
                   Characterization Facilities
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="annual-report">
+                <Link
+                  className="nav-link"
+                  aria-current="page"
+                  to="annual-report"
+                >
                   Annual Report
                 </Link>
               </li>
