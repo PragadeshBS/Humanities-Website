3564f3045014480e0508c03c43a90e9743bd785f
Pragadesh BS <pragadeshbs@protonmail.com>
2022-05-03 14:10:25 +0530

    fix file imports

--- src/routes/faculty/Teaching.js
+++ src/routes/faculty/Teaching.js
@@ -2,8 +2,8 @@ import { useEffect } from "react";
 import { getdetails } from "../../data/faculty";
 import { useState } from "react";
 import Title from "../../components/Title";
-import SideBar from "../../components/sideBsar/Sidebar";
-import Cards from "../../components/carsd/Cards";
+import SideBar from "../../components/sidebar/Sidebar";
+import Cards from "../../components/card/Cards";
 export default function Teaching() {
   useEffect(() => {
     document.title = "teaching staff";
