c9b978dc02a8f1e40c885589a13fa43140ec4bc8
Pr454th <93435120+Pr454th@users.noreply.github.com>
2022-05-09 15:44:11 +0000

    Update index.js

--- src/routes/infrastructure/index.js
+++ src/routes/infrastructure/index.js
@@ -9,15 +9,10 @@ const Infrastructure = () => {
       <div className="container mt-5 mb-5">
         <div className="row">
           <h1 className="display">INFRASTRUCTURE</h1>
-          <div className="accordion" id="accordionExample">
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="headingOne">
-                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              <h2 className="display mt-3">
                   PHYSICS LABORATORY
-                </button>
               </h2>
-              <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                <center>
+              <center>
                 <div id="carouselExampleIndicators" className="carousel slide idivision" data-bs-ride="carousel">
                   <div className="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
@@ -27,16 +22,16 @@ const Infrastructure = () => {
                   </div>
                   <div className="carousel-inner">
                     <div className="carousel-item active">
-                      <img src="static/images/infrastructure/phy_Lab_1.jpg" className="img-fluid" alt=".h."/>
+                      <img src="static/images/infrastructure/phy_Lab_1.jpg" className="img-fluid idivision" alt=".h."/>
                     </div>
                     <div className="carousel-item">
-                      <img src="static/images/infrastructure/phy_Lab_2.jpg"  className="img-fluid" alt="..."/>
+                      <img src="static/images/infrastructure/phy_Lab_2.jpg"  className="img-fluid idivision" alt="..."/>
                     </div>
                     <div className="carousel-item">
-                      <img src="static/images/infrastructure/phy_Lab_3.jpg"  className="img-fluid" alt="..."/>
+                      <img src="static/images/infrastructure/phy_Lab_3.jpg"  className="img-fluid idivision" alt="..."/>
                     </div>
                     <div className="carousel-item">
-                      <img src="static/images/infrastructure/phy_Lab_4.jpg"  className="img-fluid" alt="..."/>
+                      <img src="static/images/infrastructure/phy_Lab_4.jpg"  className="img-fluid idivision" alt="..."/>
                     </div>
                   </div>
                   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -49,15 +44,9 @@ const Infrastructure = () => {
                   </button>
                 </div>
                 </center>
-              </div>
-            </div>
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="headingTwo">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              <h2 className="display mt-5">
                   CHEMISTRY LABORATORY
-                </button>
               </h2>
-              <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
               <center>
                 <div id="carouselExampleIndicators2" className="carousel slide idivision" data-bs-ride="carousel">
                   <div className="carousel-indicators">
@@ -67,13 +56,13 @@ const Infrastructure = () => {
                   </div>
                   <div className="carousel-inner">
                     <div className="carousel-item active">
-                      <img src="static/images/infrastructure/che_Lab_1.jpg" className="img-fluid" alt="..."/>
+                      <img src="static/images/infrastructure/che_Lab_1.jpg" className="img-fluid idivision" alt="..."/>
                     </div>
                     <div className="carousel-item">
-                      <img src="static/images/infrastructure/che_Lab_2.jpg"  className="img-fluid" alt="..."/>
+                      <img src="static/images/infrastructure/che_Lab_2.jpg"  className="img-fluid idivision" alt="..."/>
                     </div>
                     <div className="carousel-item">
-                      <img src="static/images/infrastructure/che_Lab_3.jpg"  className="img-fluid" alt="..."/>
+                      <img src="static/images/infrastructure/che_Lab_3.jpg"  className="img-fluid idivision" alt="..."/>
                     </div>
                   </div>
                   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide="prev">
@@ -86,23 +75,16 @@ const Infrastructure = () => {
                   </button>
                 </div>
                 </center>
-              </div>
-            </div>
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="headingThree">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              <h2 className="display mt-5">
                   UV LABORATORY
-                </button>
-              </h2>
-              <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-              <center>
+              </h2><center>
                 <div id="carouselExampleIndicators3" className="carousel slide idivision" data-bs-ride="carousel">
                   <div className="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleIndicators3" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                   </div>
                   <div className="carousel-inner">
                     <div className="carousel-item active">
-                      <img src="static/images/infrastructure/uv_research.jpg" className="img-fluid" alt="..."/>
+                      <img src="static/images/infrastructure/uv_research.jpg" className="img-fluid idivision" alt="..."/>
                     </div>
                   </div>
                   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators3" data-bs-slide="prev">
@@ -115,15 +97,9 @@ const Infrastructure = () => {
                   </button>
                 </div>
                 </center>
-              </div>
-            </div>
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="headingThree">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                  COMMUNICATION LABORATORY
-                </button>
+              <h2 className="display mt-5">
+                COMMUNICATION LABORATORY
               </h2>
-              <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
               <center>
                 <div id="carouselExampleIndicators4" className="carousel slide idivision" data-bs-ride="carousel">
                   <div className="carousel-indicators">
@@ -132,10 +108,10 @@ const Infrastructure = () => {
                   </div>
                   <div className="carousel-inner">
                     <div className="carousel-item active">
-                      <img src="static/images/infrastructure/com_1.jpg" className="img-fluid" alt="..."/>
+                      <img src="static/images/infrastructure/com_1.jpg" className="img-fluid idivision" alt="..."/>
                     </div>
                     <div className="carousel-item">
-                      <img src="static/images/infrastructure/com_2.jpg"  className="img-fluid" alt="..."/>
+                      <img src="static/images/infrastructure/com_2.jpg"  className="img-fluid idivision" alt="..."/>
                     </div>
                   </div>
                   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators4" data-bs-slide="prev">
@@ -148,15 +124,9 @@ const Infrastructure = () => {
                   </button>
                 </div>
                 </center>
-              </div>
-            </div>
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="headingFive">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+              <h2 className="display mt-5">
                   COMPUTATIONAL MATHEMATICS LABORATORY
-                </button>
               </h2>
-              <div id="collapseFive" className="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
               <center>
                 <div id="carouselExampleIndicators5" className="carousel slide idivision" data-bs-ride="carousel">
                   <div className="carousel-indicators">
@@ -165,10 +135,10 @@ const Infrastructure = () => {
                   </div>
                   <div className="carousel-inner">
                     <div className="carousel-item active">
-                      <img src="static/images/infrastructure/com_mat_lab_1.jpeg" className="img-fluid" alt="..."/>
+                      <img src="static/images/infrastructure/com_mat_lab_1.jpeg" className="img-fluid idivision" alt="..."/>
                     </div>
                     <div className="carousel-item">
-                      <img src="static/images/infrastructure/com_mat_lab_2.jpeg"  className="img-fluid" alt="..."/>
+                      <img src="static/images/infrastructure/com_mat_lab_2.jpeg"  className="img-fluid idivision" alt="..."/>
                     </div>
                   </div>
                   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators5" data-bs-slide="prev">
@@ -181,15 +151,9 @@ const Infrastructure = () => {
                   </button>
                 </div>
                 </center>
-              </div>
-            </div>
-            <div className="accordion-item">
-              <h2 className="accordion-header" id="headingSix">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+              <h2 className="display mt-5">
                   HIGH TEMPERATURE FURNACE LABORATORY
-                </button>
               </h2>
-              <div id="collapseSix" className="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
               <center>
                 <div id="carouselExampleIndicators6" className="carousel slide idivision" data-bs-ride="carousel">
                   <div className="carousel-indicators">
@@ -197,7 +161,7 @@ const Infrastructure = () => {
                   </div>
                   <div className="carousel-inner">
                     <div className="carousel-item active">
-                      <img src="static/images/infrastructure/High_Temp_Furnace_Lab.jpg" className="img-fluid" alt="..."/>
+                      <img src="static/images/infrastructure/High_Temp_Furnace_Lab.jpg" className="img-fluid idivision" alt="..."/>
                     </div>
                   </div>
                   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators6" data-bs-slide="prev">
@@ -210,9 +174,6 @@ const Infrastructure = () => {
                   </button>
                 </div>
                 </center>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
