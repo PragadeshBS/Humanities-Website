f688dc330d356f20b706b9f16964b6f2e55aca51
Pragadesh BS <pragadeshbs@protonmail.com>
2023-03-30 17:50:26 +0530

    make dept name bold

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -14,10 +14,10 @@ const Header = () => {
             />
           </div>
           <div className="ms-3 title">
-            <span className="h1 text-center">
+            <span className="h1 text-center fw-bold">
               Department of Applied Sciences &amp; Humanities
             </span>
-            <span className="h5 text-center">MIT Campus, Anna University</span>
+            <span className="h5 text-center fw-bold">MIT Campus, Anna University</span>
           </div>
           {/* <div>
             <img src="/static/images/app/auLogo.png" alt="annauniv-logo" className="logo-img" />
