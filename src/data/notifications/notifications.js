8d29334761e53c61d63086ae9e89dff6d7bdfff7
Prasath <prasathkarthiban1999@gmail.com>
2023-06-30 11:23:31 +0530

    notification 3

--- src/data/notifications/notifications.js
+++ src/data/notifications/notifications.js
@@ -1,7 +1,12 @@
 export const notifications = [
   {
+    id: 0,
+    text: `Invited Talk for B.E students of Automobile and RPT Students on the topic "Speaking skills" - 04.07.2023`,
+    link: "/static/docs/notifications/Invited_Talk_for_BE_students_of_Automobile_and_RPT_Students.pdf",
+  },
+  {
     id: 1,
-    text: `Invited lecture for B.E students of IT and AIDS on the topic "Complex Analytic Functions and Its Applications" - 26.06.2023`,
+    text: `Invited lecture for B.E students of IT and AIDS on the topic "Complex Analytic Functions and Its Applications" - 26.06.2023`,
     link: "/static/docs/notifications/Complex_Analytic_Functions_and_Its_Applications.pdf",
   },
   {
